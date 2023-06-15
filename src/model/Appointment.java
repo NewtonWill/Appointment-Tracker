113922143b0dd6ad9ec9f05bafa0c5d6eb1ab212
APRXMT <69224310+APRXMT@users.noreply.github.com>
2023-06-14 23:52:16 -0500

    Appointment model set up

---
+++ src/model/Appointment.java
@@ -0,0 +1,112 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Appointment {
+
+    private Integer Appointment_ID;
+    private String Title;
+    private String Description;
+    private String Location;
+    private Integer ContactID;
+    private String Type;
+    private LocalDateTime StartDT;
+    private LocalDateTime EndDT;
+    private Integer Customer_ID;
+    private Integer User_ID;
+
+    public Appointment(Integer appointment_ID, String title, String description,
+                       String location, Integer contactID, String type,
+                       LocalDateTime startDT, LocalDateTime endDT, Integer customer_ID, Integer user_ID) {
+        this.Appointment_ID = appointment_ID;
+        this.Title = title;
+        this.Description = description;
+        this.Location = location;
+        this.ContactID = contactID;
+        this.Type = type;
+        this.StartDT = startDT;
+        this.EndDT = endDT;
+        this.Customer_ID = customer_ID;
+        this.User_ID = user_ID;
+    }
+
+    public Integer getAppointment_ID() {
+        return Appointment_ID;
+    }
+
+    public void setAppointment_ID(Integer appointment_ID) {
+        Appointment_ID = appointment_ID;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public Integer getContactID() {
+        return ContactID;
+    }
+
+    public void setContactID(Integer contactID) {
+        ContactID = contactID;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public LocalDateTime getStartDT() {
+        return StartDT;
+    }
+
+    public void setStartDT(LocalDateTime startDT) {
+        StartDT = startDT;
+    }
+
+    public LocalDateTime getEndDT() {
+        return EndDT;
+    }
+
+    public void setEndDT(LocalDateTime endDT) {
+        EndDT = endDT;
+    }
+
+    public Integer getCustomer_ID() {
+        return Customer_ID;
+    }
+
+    public void setCustomer_ID(Integer customer_ID) {
+        Customer_ID = customer_ID;
+    }
+
+    public Integer getUser_ID() {
+        return User_ID;
+    }
+
+    public void setUser_ID(Integer user_ID) {
+        User_ID = user_ID;
+    }
+}
